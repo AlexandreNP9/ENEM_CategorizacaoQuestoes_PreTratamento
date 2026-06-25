d0b822953107a85fa1042d0554d4742055d45233
pederpan <144904600+pederpan@users.noreply.github.com>
2026-06-25 09:23:40 -0300

    Update linux-requirements.sh

--- linux-requirements.sh
+++ linux-requirements.sh
@@ -3,14 +3,17 @@ echo "Oi, aqui fala o professor Alexandre pelo Linux"
 echo "Nao se sinta sozinho(a), eu estou no computador com voce"
 echo
 echo
+echo
 echo "vou CRIAR o ambiente virtual na pasta de nivel anterior"
 python3 -m venv ../venv &&
 echo "PRONTO!"
 echo
 echo
+echo
 echo "agora vou ATIVAR o ambiente virtual"
 source ../venv/bin/activate &&
 echo "PRONTO!"
+echo
 echo
 echo
 echo "finalmente, eu vou INSTALAR a biblioteca pdf2image"
@@ -19,12 +22,16 @@ pip3 install pdf2image &&
 echo
 echo "PRONTO!"
 echo
-echo "agora vc pode codar :) boa sorte"
+echo
+echo
+echo "vou dar um git config --list para vc conferir seu user.email, user.name, se está no seu repositório mesmo e o credential.helper"
+echo
+git config --list
 echo
 echo
 echo
-echo "lembre-se de dar o git config --list para conferir se vc esta realmente no seu repositorio"
-echo "e conferir o user.email e user.name"
+echo "==> CONFIRA <== se está tudo certo no seu user.email, user.name, remote.origin.url e o credential.helper"
+echo "se estiver tudo certo, pode codar. Boa sorte!"
 echo
 echo
 echo
